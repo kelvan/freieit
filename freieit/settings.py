aba1187a1d851623c60dd3f7ec7d571564344ea6
uniqx <uniqx@fused.at>
2012-04-14 13:07:08 +0200

    tidy settings.py a litte up

--- freieit/settings.py
+++ freieit/settings.py
@@ -54,15 +54,11 @@ USE_TZ = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 MEDIA_ROOT = os.path.dirname(os.path.dirname(__file__))+'/_media'
-print MEDIA_ROOT
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = '/media/'
-MEDIA_URL = 'http://localhost:8000/media/'
-#MEDIA_URL = ''
-print 'MEDIA_URL', MEDIA_URL
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
