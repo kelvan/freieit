1b16a688d8581f69a234aa604edefb0cd73e3bbe
uniqx <uniqx@fused.at>
2012-04-18 16:51:58 +0200

    templates

--- freieit/views/experts.py
+++ freieit/views/experts.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
+from freieit.models import ExpertProfile
 
 def show(request):
-  return render_to_response('experts.html')
+
+  expert_selection = ExpertProfile.objects.all()
+
+  return render_to_response('experts.html',{'experts': expert_selection})
+  #return render_to_response('base.html')
 
 
