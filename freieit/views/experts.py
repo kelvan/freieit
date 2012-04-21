7d0435aba92dda11d39bed614aec8bfac325a9d7
Florian Schweikert <kelvan@logic.at>
2012-04-21 22:07:13 +0200

    urls regex merged

--- freieit/views/experts.py
+++ freieit/views/experts.py
@@ -5,7 +5,8 @@ from django.core.paginator import Paginator
 from freieit.models import ExpertProfile
 
 def show(request, page_num=1):
-
+  if not page_num:
+    page_num=1
   expert_selection = ExpertProfile.objects.all()
 
   experts_per_page = 1
@@ -13,6 +14,3 @@ def show(request, page_num=1):
 
   page={'current': int(page_num), 'last': p.num_pages}
   return render_to_response('experts.html',{'experts': p.page(page_num), 'page': page})
-  #return render_to_response('base.html')
-
-
