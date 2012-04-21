937eb94903697517bd9fc1c8774d82bb1e6d1710
Florian Schweikert <kelvan@logic.at>
2012-04-21 21:01:27 +0200

    implement pages

--- freieit/views/experts.py
+++ freieit/views/experts.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from django.core.paginator import Paginator
 
 from freieit.models import ExpertProfile
 
-def show(request):
+def show(request, page_num=1):
 
   expert_selection = ExpertProfile.objects.all()
 
-  return render_to_response('experts.html',{'experts': expert_selection})
+  experts_per_page = 1
+  p = Paginator(expert_selection, experts_per_page)
+
+  page={'current': int(page_num), 'last': p.num_pages}
+  return render_to_response('experts.html',{'experts': p.page(page_num), 'page': page})
   #return render_to_response('base.html')
 
 
