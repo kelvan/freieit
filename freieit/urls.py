2e254d7c5e68c78dd31b63d08d559f97da6f9d30
uniqx <uniqx@fused.at>
2012-04-25 21:51:41 +0200

    urls.py fixed, admin works again

--- freieit/urls.py
+++ freieit/urls.py
@@ -31,10 +31,10 @@ urlpatterns = patterns('',
   url(r'^tag/(?P<tag>\w+)/(?P<page_num>\d+)?', 'freieit.views.tag.show'),
 
   # Uncomment the admin/doc line below to enable admin documentation:
-  url(r'^admin/doc/$', include('django.contrib.admindocs.urls')),
+  url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
   # Uncomment the next line to enable the admin:
-  url(r'^admin/$', include(admin.site.urls)),
+  url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG:
