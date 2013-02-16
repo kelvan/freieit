142ecb37a1801a70540905345a04707620ec7c4d
Albert Dengg <albert@fsfe.org>
2013-02-16 02:49:50 +0100

    unify local & remote functions

--- fabfile.py
+++ fabfile.py
@@ -1,78 +1,95 @@
+#!/usr/bin/env python
+
 from fabric.api import *
-#from fabric.contrib import django
+from contextlib import contextmanager as _contextmanager
+from sys import executable as sys_executable
 
-#django.project('freieit')
-#from freieit.models import ExpertProfile
+@_contextmanager
+def virtualenv():
+    with env.cd(env.directory):
+        with prefix(env.activate):
+            yield
 
-def production():
-    env.hosts = ['freie.it']
-    env.directory = 'freieit'
-    env.user = 'kelvan'
-    env.deploy_user = 'kelvan'
-    env.activate = '. /home/kelvan/.virtualenvs/freieit/bin/activate'
 
-def dev():
-    env.directory = '.'
-    env.activate = '. env/bin/activate'
+def prep():
+    if env.host_string == 'localhost':
+        env.directory = '.'
+        env.activate = '. env/bin/activate'
+        env.pyexecutable = sys_executable
+        env.cd = lcd
+        env.run = local
+    else:
+        #env.hosts = ['freie.it']
+        env.directory = 'freieit'
+        env.user = 'kelvan'
+        env.deploy_user = 'kelvan'
+        env.activate = '. /home/kelvan/.virtualenvs/freieit/bin/activate'
+        env.cd = cd
+        env.pyexecutable = 'python'
+
+def make_venv():
+    prep()
+    with env.cd(env.directory):
+        env.run('virtualenv --system-site-packages -p %s env' % (env.pyexecutable, ))
 
 def vars():
     env.app= 'freieit'
 
-def virtualenv(command):
-    with cd(env.directory):
-        run(env.activate + '&&' + command)
-
-def lvirtualenv(command):
-    with lcd(env.directory):
-        local(env.activate + '&&' + command)
-
-def make_venv():
-    dev()
-    with lcd(env.directory):
-        local('virtualenv --system-site-packages -p python2.7 env')
-
 def install_req():
-    dev()
-    lvirtualenv('pip install -U -r requirements.txt')
+    prep()
+    with virtualenv():
+        env.run('pip install -U -r requirements.txt')
 
 def runserver():
-    dev()
-    lvirtualenv('python manage.py runserver')
+    prep()
+    with virtualenv():
+        env.run('python manage.py runserver')
 
 def syncdb():
-    dev()
-    lvirtualenv('python manage.py syncdb')
-    lvirtualenv('python manage.py migrate freieit')
+    prep()
+    with virtualenv():
+        env.run('python manage.py syncdb')
+        env.run('python manage.py migrate freieit')
 
 def clean():
-    dev()
-    with lcd(env.directory):
-        local('rm -f _freieit.db')
-        local('rm -rf _media/')
+    prep()
+    with env.cd(env.directory):
+        env.run('rm -f _freieit.db')
+        env.run('rm -rf _media/')
 
 def fixtures():
-    dev()
+    prep()
     print ExpertProfile.objects.all()[0]
 
 def prepare_migration():
-    dev()
-    with lcd(env.directory):
-        vars()
-        env.warn_only = True
-        lvirtualenv('python manage.py schemamigration %s --auto' % env.app)
-        env.warn_only = False
-        local('hg add %s/migrations' % env.app)
+    prep()
+    with env.cd(env.directory):
+        with virtualenv():
+            vars()
+            env.warn_only = True
+            env.run('python manage.py schemamigration %s --auto' % env.app)
+            env.warn_only = False
+            env.run('hg add %s/migrations' % env.app)
 
 def pull_update():
-    with cd(env.directory):
-        run('hg pull')
-        run('hg update')
+    prep()
+    with env.cd(env.directory):
+        env.run('hg pull')
+        env.run('hg update')
 
 def deploy():
+    prep()
     pull_update()
-    virtualenv('python manage.py syncdb --noinput')
-    virtualenv('python manage.py migrate freieit')
+    with virtualenv():
+        env.run('python manage.py syncdb --noinput')
+        env.run('python manage.py migrate freieit')
 
 def shell():
-    dev()
-    lvirtualenv('python manage.py shell')
+    prep()
+    with virtualenv():
+        env.run('python manage.py shell')
+
+def test_venv():
+    prep()
+    with virtualenv():
+        env.run('python -c "import sys; print sys.path"')
