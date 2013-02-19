2c656440d2578644d064903376be404f2f96a5da
Florian Schweikert <kelvan@logic.at>
2013-02-19 13:45:55 +0100

    fixing static links
    don't forget to run python manage.py collectstatic

--- freieit/settings.py
+++ freieit/settings.py
@@ -25,6 +25,9 @@ DATABASES = {
     }
 }
 
+APPDIR = os.path.dirname(__file__)
+PROJDIR = os.path.dirname(APPDIR)
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -53,7 +56,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.dirname(os.path.dirname(__file__))+'/_media'
+MEDIA_ROOT = os.path.join(PROJDIR, '/_media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -64,7 +67,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.dirname(os.path.dirname(__file__))+'/_static_dump'
+STATIC_ROOT = os.path.join(PROJDIR, '_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -75,7 +78,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-
+    os.path.join(APPDIR, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -172,7 +175,7 @@ LOGGING = {
 
 HAYSTACK_SITECONF = 'freieit.search_indexes'
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
-HAYSTACK_WHOOSH_PATH = os.path.join(os.path.dirname(__file__), 'whoosh_index')
+HAYSTACK_WHOOSH_PATH = os.path.join(APPDIR, 'whoosh_index')
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 2525
