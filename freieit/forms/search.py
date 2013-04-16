ca09a30db987a487eb744a0552335666fed52bfb
gregor <devnull@localhost>
2013-04-16 18:58:30 +0200

    BUG:no search result with empty search string. hacky workaround

--- freieit/forms/search.py
+++ freieit/forms/search.py
@@ -1,6 +1,8 @@
 from haystack.forms import SearchForm
 from django import forms
 import random
+from freieit import models
+from haystack.models import SearchResult
 
 class ExpertSearchForm(SearchForm):
     q = forms.CharField(required=False,
@@ -24,7 +26,23 @@ class ExpertSearchForm(SearchForm):
             errors_on_separate_row = True)
 
     def no_query_found(self):
-        all_ = list(self.searchqueryset.all())        
+        # we do not use searchqueryset here because with ngramfields
+        # all does not return any object
+        # see https://groups.google.com/forum/?fromgroups#!topic/django-haystack/q1n6w6S5PJQ
+        
+        # this a quick hack: fake a searchresult list
+        # Performance? (Double database access, sort as list in memory)
+        # Since we have to switch the search backend anyway if the site grows
+        # this should work as quick workaround
+        # (premature optimization ist the root of all evil)
+        
         random.seed(self.seed)
-        all_.sort(key=lambda o:random.random())
+        def make_result(expert):
+            return SearchResult(app_label="freieit",
+                                pk=expert.id,
+                                model_name="expertprofile",
+                                score=random.random())
+        all_ = map(make_result,
+                   models.ExpertProfile.objects.filter(available=True))
+        all_.sort(key=lambda o:o.score)
         return all_
--- freieit/search_indexes.py
+++ freieit/search_indexes.py
@@ -2,6 +2,7 @@ from haystack import indexes, site
 from freieit.models import ExpertProfile
 
 class ExpertProfileIndex(indexes.SearchIndex):
+    #text = indexes.CharField(document=True, use_template=True)
     text = indexes.EdgeNgramField(document=True, use_template=True)
     #name = indexes.CharField(model_attr='name')
     #location = indexes.CharField(model_attr='location')
