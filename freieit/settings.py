da1f40ec72df5b1dea48746728a1805a700d3cd7
Florian Schweikert <kelvan@ist-total.org>
2016-03-15 17:21:08 +0100

    Disable threaded_multihost middleware

--- freieit/settings.py
+++ freieit/settings.py
@@ -99,7 +99,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'threaded_multihost.middleware.ThreadLocalMiddleware',
+    #'threaded_multihost.middleware.ThreadLocalMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
