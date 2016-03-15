67846517a5addaa11809c4c5a8b48e9d342a3fb8
Florian Schweikert <kelvan@ist-total.org>
2016-03-15 17:25:51 +0100

    Fix imports

--- freieit/forms/expert.py
+++ freieit/forms/expert.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 
-from .models import ExpertProfile
+from ..models import ExpertProfile
 
 
 class ExpertProfileForm(ModelForm):
--- freieit/views/newexpert.py
+++ freieit/views/newexpert.py
@@ -5,7 +5,7 @@ from django.template.loader import get_template
 from django.template import Context
 from django.conf import settings
 
-from .forms.expert import ExpertProfileForm
+from ..forms.expert import ExpertProfileForm
 
 
 def show(request):
