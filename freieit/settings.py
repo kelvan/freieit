ab285cdd44a975a7180b5228c21efee2bfd2fe5a
Florian Schweikert <kelvan@ist-total.org>
2016-09-28 17:47:31 +0200

    Move locale folder

--- freieit/settings.py
+++ freieit/settings.py
@@ -18,8 +18,7 @@ DATABASES = {
     }
 }
 
-APPDIR = os.path.dirname(__file__)
-PROJDIR = os.path.dirname(APPDIR)
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -47,9 +46,11 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
+LOCALE_PATHS = (os.path.join(BASE_DIR, 'locale'),)
+
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PROJDIR, '_media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +61,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJDIR, '_static')
+STATIC_ROOT = os.path.join(BASE_DIR, '_static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -166,7 +167,7 @@ LOGGING = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(PROJDIR, 'whoosh_index'),
+        'PATH': os.path.join(BASE_DIR, 'whoosh_index'),
     },
 }
 
